f034e24639ba98b309a9ac2261742bb822f5a229
Quentin Thierry <quentin.thierry@ens.uvsq.fr>
2022-02-01 22:54:19 +0100

    Added win condition to alice and bob, alice wins every time while learning

--- AlgoJeu.py
+++ AlgoJeu.py
@@ -41,7 +41,11 @@ def take_action(etat, Q, probExploration):
         action = np.argmax(Q[etat])
     return action
 
-def algo(alice_card, alice_bet_amount, alice_money, total_bet, bob_money, bob_bet=1):
+def def_bob_bet():
+    return randrange(0,2)
+
+
+def algo(alice_card, alice_bet_amount, alice_money, total_bet, bob_money, bob_bet):
     # random card draw
     
     bob_card = randrange(1,11)
@@ -60,158 +64,98 @@ def algo(alice_card, alice_bet_amount, alice_money, total_bet, bob_money, bob_be
             if alice_card > bob_card: # alice wins
                 alice_money += total_bet
                 total_bet = 0
-                return 1
+                return 1, alice_money, bob_money, total_bet
                 
             elif alice_card < bob_card: # bob wins
                 bob_money += total_bet
                 total_bet = 0
-                return -1
+                return -1, alice_money, bob_money, total_bet
         else:
             alice_money += total_bet # alice wins by default -> bob lay down
             total_bet = 0
             if alice_card > bob_card: # alice wins
-                return 1
+                return 1, alice_money, bob_money, total_bet
             else:
-                return -1
+                return -1, alice_money, bob_money, total_bet
     else:
         bob_money += total_bet # alice loses by default -> alice lay down
         total_bet = 0
         if alice_card > bob_card: # alice wins
-            return -1
+            return -1, alice_money, bob_money, total_bet
         else:
-            return 1
+            return 1, alice_money, bob_money, total_bet
     
-    return 0
-
+    return 0, alice_money, bob_money, total_bet
 
 
 QALICE = [
-    [-1, 0, 0],   #0
-    [-1, 0, 0],   #1
-    [-1, 0, 0],   #2
-    [-1, 0, 0],   #3
-    [-1, 0, 0],   #4
-    [-1, 0, 0],   #5
-    [-1, 0, 0],   #6
-    [-1, 0, 0],   #7
-    [-1, 0, 0],   #8
-    [-1, 0, 0]    #9
+    [0, 0, 0],   #0
+    [0, 0, 0],   #1
+    [0, 0, 0],   #2
+    [0, 0, 0],   #3
+    [0, 0, 0],   #4
+    [0, 0, 0],   #5
+    [0, 0, 0],   #6
+    [0, 0, 0],   #7
+    [0, 0, 0],   #8
+    [0, 0, 0]    #9
 ]
 
+start = 0
+end = 0
+iterations = 1000
+res = []
+iters = []
+test = []
 
-alice_money = 100
-bob_money = 100
-alice_bet_amount = 0
-total_bet = 0
+for i in tqdm(range(iterations)):
+    
+    alice_money = 100
+    bob_money = 100
+    alice_bet_amount = 0
+    total_bet = 0
+    start += time.time()
 
-for _ in tqdm(range(100000000)):
-    etat = random_card()
+    while alice_money > 0 and bob_money > 0:
+        
+        etat = random_card()
 
-    act = take_action(etat-1, QALICE, 0.01)
+        act = take_action(etat-1, QALICE, 0.1)
 
-    alice_bet_amount = choose_alice_action(act)
+        alice_bet_amount = choose_alice_action(act)
 
-    r = algo(etat-1, alice_bet_amount, alice_money, total_bet, bob_money)
+        r, alice_money, bob_money, total_bet = algo(etat-1, alice_bet_amount, alice_money, total_bet, bob_money, def_bob_bet())
+        
+        QALICE[etat-1][act] = QALICE[etat-1][act] + 0.1*(r - QALICE[etat-1][act])
+        
+    if alice_money > bob_money:
+        res.append("Alice")
+        test.append(res.count("Alice"))
+    else:
+        res.append("Bob")
+    
+    iters.append(i)
+    
+    end += time.time()
 
-    QALICE[etat-1][act] = QALICE[etat-1][act] + 0.1*(r - QALICE[etat-1][act])
+
 
 def afficher_table():
     
     for s in range(0, 10):
-        print(s, round(QALICE[s][0],4), round(QALICE[s][1],4), round(QALICE[s][2],4))
+        print(s+1, round(QALICE[s][0],4), round(QALICE[s][1],4), round(QALICE[s][2],4))
 
 afficher_table()
 
+print("Number of games :", iterations)
+print("Average time of one game :", round((end - start)/iterations, 5), 's')
+
+value_a = res.count("Alice")
+value_b = res.count("Bob")
 
 
+print("Alice wins :", value_a)
+print("Bob wins :", value_b)
 
-
-# # 0 : lay down, 1 : bet 1€, 2 : bet 3€
-# def alice_bet(alice_money):
-#     x = randrange(0,3)
-#     if x == 2:
-#         x = 3
-    
-#     if (alice_money - x) >= 0:
-#         return x
-#     else:
-#         if (alice_money - 1) <= -1:
-#             return 0
-#         else:
-#             return 1
-
-# # 0 : lay down, 1 : follow alice
-# def bob_bet():
-#     return randrange(0,2)
-    
-
-# start = 0
-# end = 0
-
-# res = []
-
-# iterations = 1000
-
-# for i in tqdm(range(iterations)):
-#     start += time.time()
-    
-#     alice_money = 100
-#     bob_money = 100
-#     alice_bet_amount = 0
-#     total_bet = 0
-
-#     while alice_money > 0 and bob_money > 0:
-        
-#         # random card draw
-#         alice_card = randrange(1,11)
-        
-#         alice_bet_amount = alice_bet(alice_money)
-
-#         if alice_bet_amount != 0: # if alice doesnt lay down
-#             total_bet += alice_bet_amount # add alice bet to total
-#             alice_money -= alice_bet_amount
-#             bob_card = randrange(1,11)
-            
-#             if bob_bet() != 0: # if bob doesnt lay down
-#                 total_bet += alice_bet_amount
-#                 bob_money -= alice_bet_amount
-#                 if bob_money < 0:
-#                     total_bet += bob_money
-#                     bob_money = 0
-
-#                 if alice_card > bob_card: # alice wins
-#                     alice_money += total_bet
-#                     total_bet = 0
-                    
-#                 elif alice_card < bob_card: # bob wins
-#                     bob_money += total_bet
-#                     total_bet = 0
-#             else:
-#                 alice_money += total_bet
-#                 total_bet = 0
-#         else:
-#             bob_money += total_bet
-#             total_bet = 0
-            
-#     end += time.time()
-    
-#     if alice_money <= 0:
-#         res.append("Bob")
-#     else:
-#         res.append("Alice")
-
-
-# value_a = res.count("Alice")
-# value_b = res.count("Bob")
-
-# values = [value_a, value_b]
-# names = ['Alice', 'Bob']
-
-# plt.bar(names, values)
-# plt.show()
-
-# print("Alice won", value_a, "times.")
-# print("Bob won", value_b, "times.")
-# print("Number of games :", iterations)
-# print("Average time of one game :", round((end - start)/iterations, 5), 's')
-
+plt.scatter(iters , test, c="r", cmap='summer')
+plt.show()
